8c7f3b91566b9bb5c960d2e6021dde7e03c5e788
Calm <chenzo0220@163.com>
2025-10-20 17:21:51 +0800

    Basic Table2,3

--- src/app/(admin)/(others-pages)/(tables)/basic-tables/page.tsx
+++ src/app/(admin)/(others-pages)/(tables)/basic-tables/page.tsx
@@ -1,6 +1,9 @@
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import BasicTableOne from "@/components/tables/BasicTableOne";
+import BasicTableTwo from "@/components/tables/BasicTableTwo";
+import BasicTableThree from "@/components/tables/BasicTableThree";
+
 import { Metadata } from "next";
 import React from "react";
 
@@ -18,6 +21,12 @@ export default function BasicTables() {
       <div className="space-y-6">
         <ComponentCard title="Basic Table 1">
           <BasicTableOne />
+        </ComponentCard>
+        <ComponentCard title="Basic Table 2">
+          <BasicTableTwo />
+        </ComponentCard>
+        <ComponentCard title="Basic Table 3">
+          <BasicTableThree />
         </ComponentCard>
       </div>
     </div>
