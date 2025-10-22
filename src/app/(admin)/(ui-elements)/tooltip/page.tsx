6b444a4d96793f7bfc10228d3083f01ca567ca0f
Calm <chenzo0220@163.com>
2025-10-22 10:13:59 +0800

    ui+tooltip

--- src/app/(admin)/(ui-elements)/tooltip/page.tsx
+++ src/app/(admin)/(ui-elements)/tooltip/page.tsx
@@ -1,20 +1,32 @@
-
+import ComponentCard from "@/components/common/ComponentCard";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import DefaultTooltip from "@/components/ui/tooltip/DefaultTooltip";
+import TooltipPlacement from "@/components/ui/tooltip/TooltipPlacement";
+import WhiteAndDarkTooltip from "@/components/ui/tooltip/WhiteAndDarkTooltip";
 import { Metadata } from "next";
 import React from "react";
-import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import TooltipExample from "@/components/ui/tooltip";
 
 export const metadata: Metadata = {
-  title: "Next.js Tags | TailAdmin - Next.js Dashboard Template",
+  title: "Next.js Tooltips | TailAdmin - Next.js Dashboard Template",
   description:
-    "This is Next.js Tags page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "This is Next.js Tooltips page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
-export default function TagsPage() {
+export default function Tooltips() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="Tags" />
-      <TooltipExample />   
+      <PageBreadcrumb pageTitle="Tooltip" />
+      <div className="space-y-5 sm:space-y-6">
+        <ComponentCard title="Default Tooltip">
+          <DefaultTooltip />
+        </ComponentCard>
+        <ComponentCard title="White and Dark Tooltip">
+          <WhiteAndDarkTooltip />
+        </ComponentCard>
+        <ComponentCard title="Tooltip Placement">
+          <TooltipPlacement />
+        </ComponentCard>
+      </div>
     </div>
   );
 }
