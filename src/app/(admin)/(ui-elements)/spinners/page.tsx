ca5991578d4efb7881be74f7abe63a914ce11047
Calm <chenzo0220@163.com>
2025-10-22 10:06:43 +0800

    ui+spinners

--- src/app/(admin)/(ui-elements)/spinners/page.tsx
+++ src/app/(admin)/(ui-elements)/spinners/page.tsx
@@ -1,8 +1,12 @@
-
-import { Metadata } from "next";
-import React from "react";
+import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import SpinnerExample from "@/components/ui/spinner";
+import React from "react";
+
+import SpinnerTwo from "./SpinnerTwo";
+import SpinnerThree from "./SpinnerThree";
+import SpinnerFour from "./SpinnerFour";
+import { Metadata } from "next";
+import SpinnerOne from "./SpinnerOne";
 
 export const metadata: Metadata = {
   title: "Next.js Spinners | TailAdmin - Next.js Dashboard Template",
@@ -10,11 +14,24 @@ export const metadata: Metadata = {
     "This is Next.js Spinners page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
-export default function SpinnersPage() {
+export default function Spinners() {
   return (
     <div>
       <PageBreadcrumb pageTitle="Spinners" />
-      <SpinnerExample />   
+      <div className="space-y-5 sm:space-y-6">
+        <ComponentCard title="Spinner 1">
+          <SpinnerOne />
+        </ComponentCard>{" "}
+        <ComponentCard title="Spinner 2">
+          <SpinnerTwo />
+        </ComponentCard>{" "}
+        <ComponentCard title="Spinner 3">
+          <SpinnerThree />
+        </ComponentCard>{" "}
+        <ComponentCard title="Spinner 4">
+          <SpinnerFour />
+        </ComponentCard>
+      </div>
     </div>
   );
 }
