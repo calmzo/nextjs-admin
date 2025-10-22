2813f41fadfd23f06d857b60196f12bcfdcd54f5
Calm <chenzo0220@163.com>
2025-10-22 09:23:06 +0800

    ui+links

--- src/app/(admin)/(ui-elements)/links/page.tsx
+++ src/app/(admin)/(ui-elements)/links/page.tsx
@@ -1,20 +1,27 @@
-
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import ColoredLinkWithUnderline from "@/components/ui/links/ColoredLinkWithUnderline";
+import DefaultLinkExample from "@/components/ui/links/DefaultLinkExample";
+import LinkOpacityExample from "@/components/ui/links/LinkOpacityExample";
+import LinkOpacityHover from "@/components/ui/links/LinkOpacityHover";
 import { Metadata } from "next";
 import React from "react";
-import LinksExample from "@/components/links";
 
 export const metadata: Metadata = {
-  title: "Next.js Links | TailAdmin - Next.js Dashboard Template",
+  title: "Next.js Link | TailAdmin - Next.js Dashboard Template",
   description:
-    "This is Next.js Links page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "This is Next.js Link page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
 };
 
-export default function LinksPage() {
+export default function Links() {
   return (
-    <>
+    <div>
       <PageBreadcrumb pageTitle="Links" />
-      <LinksExample />
-    </>
+      <div className="grid grid-cols-1 gap-5 sm:gap-6 lg:grid-cols-2">
+        <DefaultLinkExample />
+        <ColoredLinkWithUnderline />
+        <LinkOpacityExample />
+        <LinkOpacityHover />
+      </div>
+    </div>
   );
 }
