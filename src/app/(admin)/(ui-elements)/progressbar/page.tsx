32f232c0369c1fa47f676d464cb400579df87f23
Calm <chenzo0220@163.com>
2025-10-22 09:57:25 +0800

    ui+progressbar

--- src/app/(admin)/(ui-elements)/progressbar/page.tsx
+++ src/app/(admin)/(ui-elements)/progressbar/page.tsx
@@ -1,20 +1,37 @@
-
+import ComponentCard from "@/components/common/ComponentCard";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import DefaultProgressbarExample from "@/components/progress-bar/DefaultProgressbarExample";
+import ProgressBarInMultipleSizes from "@/components/progress-bar/ProgressBarInMultipleSizes";
+import ProgressBarWithInsideLabel from "@/components/progress-bar/ProgressBarWithInsideLabel";
+import ProgressBarWithOutsideLabel from "@/components/progress-bar/ProgressBarWithOutsideLabel";
 import { Metadata } from "next";
 import React from "react";
-import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import ProgressBarExample from "@/components/ui/progressbar";
 
 export const metadata: Metadata = {
-  title: "Next.js ProgressBar | TailAdmin - Next.js Dashboard Template",
+  title: "Next.js Progress | TailAdmin - Next.js Dashboard Template",
   description:
-    "This is Next.js ProgressBar page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "This is Next.js Progress page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+  // other metadata
 };
 
-export default function ProgressBarPage() {
+export default function page() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="ProgressBar" />
-      <ProgressBarExample />  
+      <PageBreadcrumb pageTitle="Progressbar" />
+      <div className="space-y-5 sm:space-y-6">
+        <ComponentCard title="Default Progress Bar">
+          <DefaultProgressbarExample />
+        </ComponentCard>
+        <ComponentCard title="Progress Bar In Multiple Sizes">
+          <ProgressBarInMultipleSizes />
+        </ComponentCard>
+        <ComponentCard title="Progress Bar with Outside Label">
+          <ProgressBarWithOutsideLabel />
+        </ComponentCard>
+        <ComponentCard title="Progress Bar with Inside Label">
+          <ProgressBarWithInsideLabel />
+        </ComponentCard>
+      </div>
     </div>
   );
 }
--- src/components/progress-bar/ProgressBar.tsx
+++ src/components/progress-bar/ProgressBar.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type ProgressBarProps = {
   progress: number;
   size?: "sm" | "md" | "lg" | "xl";
